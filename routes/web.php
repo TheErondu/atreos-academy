c44d1baac900c9c9512e99dd3ad310b16e6d1985
TheErondu <erone007@gmail.com>
2024-01-23 19:14:13 +0100

    fixed route name

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('main');
 
 Auth::routes();
 Route::group(['middleware' => ['auth']], function () {
