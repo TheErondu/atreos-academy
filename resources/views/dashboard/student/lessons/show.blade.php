e57ff265b9059e997cd2651c1c956758e8aea30f
TheErondu <erone007@gmail.com>
2024-02-29 08:03:47 +0100

    added pdf preview

--- resources/views/dashboard/student/lessons/show.blade.php
+++ resources/views/dashboard/student/lessons/show.blade.php
@@ -9,39 +9,39 @@
 @section('content')
     <div class="pt-32pt">
         <div
-        class="d-flex flex-column flex-md-row align-items-center align-items-md-start flex mb-16pt text-center text-md-left">
-        <div class="flex">
-            <h1 class="h2 measure-lead-max mb-16pt">{{ $lesson->title }}
-            </h1>
-            <div class="d-flex align-items-center">
-                <div class="mr-16pt">
-                    {{-- <a href="teacher-profile.html" class="card-title">Eddie Bryan</a> --}}
-                    <div class="d-flex align-items-center">
-                        <small class="text-50 mr-2">
-                        </small>
-                    </div>
-                    <div class="flex mr-2">
-                        <a href="#" class="btn btn-light btn-sm">
-                            <i class="material-icons icon--left">date_range</i> Started:
-                            {{ \Carbon\Carbon::parse(enrollmentData($enrollment, $lesson)->lessonData->startDate)->format('jS F Y H:i:s') }}
-                        </a>
-                    </div>
-                </div>
-                @if (enrollmentData($enrollment, $lesson)->lessonData->completedDate !== null)
-                    <div class="flex mr-2">
+            class="d-flex flex-column flex-md-row align-items-center align-items-md-start flex mb-16pt text-center text-md-left">
+            <div class="flex">
+                <h1 class="h2 measure-lead-max mb-16pt">{{ $lesson->title }}
+                </h1>
+                <div class="d-flex align-items-center">
+                    <div class="mr-16pt">
+                        {{-- <a href="teacher-profile.html" class="card-title">Eddie Bryan</a> --}}
+                        <div class="d-flex align-items-center">
+                            <small class="text-50 mr-2">
+                            </small>
+                        </div>
                         <div class="flex mr-2">
-                            <a href="#" style="color: white !important;background-color:green"
-                                class="btn btn-light btn-sm">
-                                <i class="material-icons icon--left">check_circle</i> Completed:
-                                {{ \Carbon\Carbon::parse(enrollmentData($enrollment, $lesson)->lessonData->completedDate)->format('jS F Y H:i:s') }}
+                            <a href="#" class="btn btn-light btn-sm">
+                                <i class="material-icons icon--left">date_range</i> Started:
+                                {{ \Carbon\Carbon::parse(enrollmentData($enrollment, $lesson)->lessonData->startDate)->format('jS F Y H:i:s') }}
                             </a>
                         </div>
                     </div>
-                @endif
-            </div>
+                    @if (enrollmentData($enrollment, $lesson)->lessonData->completedDate !== null)
+                        <div class="flex mr-2">
+                            <div class="flex mr-2">
+                                <a href="#" style="color: white !important;background-color:green"
+                                    class="btn btn-light btn-sm">
+                                    <i class="material-icons icon--left">check_circle</i> Completed:
+                                    {{ \Carbon\Carbon::parse(enrollmentData($enrollment, $lesson)->lessonData->completedDate)->format('jS F Y H:i:s') }}
+                                </a>
+                            </div>
+                        </div>
+                    @endif
+                </div>
 
+            </div>
         </div>
-    </div>
     </div>
 
     <!-- BEFORE Page Content -->
@@ -64,7 +64,13 @@
                     </p>
                     <div class="form-group mb-32pt">
 
-
+                        @if ($lesson->document)
+                            <iframe
+                                src="http://docs.google.com/gview?url={{ asset('storage/lesson_documents/' . $lesson->document) }}&embedded=true"
+                                style="width:600px; height:400px;" frameborder="0">
+                            </iframe>
+                        @else
+                        @endif
                         <div class="d-flex align-items-center">
                             @if ($lesson->document)
                                 <a href="{{ asset('storage/lesson_documents/' . $lesson->document) }}"
@@ -75,10 +81,12 @@
                                 <span class="text-muted">No document available for download.</span>
                             @endif
                         </div>
+
                         <div style="padding-top: 0.9rem;padding-bottom: 0.9rem">
                             @if (enrollmentData($enrollment, $lesson)->lessonData->completedDate == null)
                                 <a href="{{ route('lessons.complete', ['enrollment' => $enrollment, 'lessonId' => $lesson->id]) }}"
-                                    type="submit" class="btn btn-outline-secondary mb-24pt mb-sm-0">Mark lesson as completed
+                                    type="submit" class="btn btn-outline-secondary mb-24pt mb-sm-0">Mark lesson as
+                                    completed
                                 </a>
                             @endif
                             @if (enrollmentData($enrollment, $lesson)->lessonData->completedDate !== null)
@@ -96,15 +104,15 @@
                         <div class="page-separator__text"> <i class="material-icons">ondemand_video</i> &nbsp; Video</div>
                     </div>
                     @if ($lesson->video_url)
-                    <div class="card">
-                        <div class="embed-responsive embed-responsive-16by9">
-                            <iframe class="embed-responsive-item"
-                                src="{{$lesson->video_url}}"
-                                allowfullscreen=""></iframe>
+                        <div class="card">
+                            <div class="embed-responsive embed-responsive-16by9">
+                                <iframe class="embed-responsive-item" src="{{ $lesson->video_url }}"
+                                    allowfullscreen=""></iframe>
+                            </div>
                         </div>
-                    </div>
                     @else
-                    <i style="color: #dcd92a" class="material-icons">not_interested</i> &nbsp;<span>Video content not available for this lesson</span>
+                        <i style="color: #dcd92a" class="material-icons">not_interested</i> &nbsp;<span>Video content not
+                            available for this lesson</span>
                     @endif
 
                 </div>
