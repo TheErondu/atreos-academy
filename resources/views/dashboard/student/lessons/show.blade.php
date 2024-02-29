26eac9200f08684e297e55ce358c280de132e2cb
TheErondu <erone007@gmail.com>
2024-02-29 08:20:13 +0100

    updates

--- resources/views/dashboard/student/lessons/show.blade.php
+++ resources/views/dashboard/student/lessons/show.blade.php
@@ -65,12 +65,24 @@
                     <div class="form-group mb-32pt">
 
                         @if ($lesson->document)
-                            <iframe
-                                src="https://docs.google.com/gview?url={{ asset('storage/lesson_documents/' . $lesson->document) }}&embedded=true"
-                                style="width:600px; height:400px;" frameborder="0">
-                            </iframe>
+                        <div id="adobe-dc-view" style="height: 360px; width: 500px;"></div>
+                        <script src="https://acrobatservices.adobe.com/view-sdk/viewer.js"></script>
+                        <script type="text/javascript">
+                          document.addEventListener("adobe_dc_view_sdk.ready", function(){
+                            var adobeDCView = new AdobeDC.View({clientId: "d4c8d5e379c347a1801a1be5ba50f794", divId: "adobe-dc-view"});
+                            adobeDCView.previewFile({
+                              content:{ location:
+                                { url: "{{ asset('storage/lesson_documents/' . $lesson->document) }}"}},
+                              metaData:{fileName: "Bodea Brochure.pdf"}
+                            },
+                            {
+                              embedMode: "SIZED_CONTAINER"
+                            });
+                          });
+                        </script>
                         @else
                         @endif
+                        <br>
                         <div class="d-flex align-items-center">
                             @if ($lesson->document)
                                 <a href="{{ asset('storage/lesson_documents/' . $lesson->document) }}"
